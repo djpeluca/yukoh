e12fca8367bc9945ddef9601c3c5bde5befa0c16
djpeluca <adrianbs@gmail.com>
2014-07-15 23:47:22 -0300

    changes lightbox and grid

--- index.php
+++ index.php
@@ -55,11 +55,11 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-3 col-sm-4 col-xs-6 col-sm-offset-2"><a title="Image 1" href="#"><img class="thumbnail img-responsive" src="http://placehold.it/600x350"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 2" href="#"><img class="thumbnail img-responsive" src="http://b.vimeocdn.com/ts/296/177/296177693_295.jpg"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 2" href="#"><img class="thumbnail img-responsive" src="http://placehold.it/600x350/992233"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 3" href="#"><img class="thumbnail img-responsive" src="http://placehold.it/600x350/449955/FFF"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6 col-sm-offset-2"><a title="Image 4" href="#"><img class="thumbnail img-responsive" src="http://placehold.it/600x350/992233"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 5" href="#"><img class="thumbnail img-responsive" src="http://placehold.it/600x350/2255EE"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 6" href="#"><img class="thumbnail img-responsive" src="http://placehold.it/600x350/449955/FFF"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 6" href="#"><img class="thumbnail img-responsive" src="http://placehold.it/600x350"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6 col-sm-offset-2"><a title="Image 8" href="#"><img class="thumbnail img-responsive" src="http://placehold.it/600x350/777"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 9" href="#"><img class="thumbnail img-responsive" src="http://placehold.it/600x350/992233"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 10" href="#"><img class="thumbnail img-responsive" src="http://placehold.it/600x350/EEE"></a></div>
@@ -72,10 +72,7 @@
 	<div class="modal" id="myModal" role="dialog">
 		  <div class="modal-dialog">
 		  <div class="modal-content">
-			<div class="modal-header">
-				<button class="close" type="button" data-dismiss="modal">×</button>
-				<h3 class="modal-title"></h3>
-			</div>
+			
 			<div class="modal-body">
 				<div id="modalCarousel" class="carousel">
 		 
@@ -88,9 +85,7 @@
 				  
 				</div>
 			</div>
-			<div class="modal-footer">
-				<button class="btn btn-default" data-dismiss="modal">Close</button>
-			</div>
+			
 		   </div>
 		  </div>
 	</div>
