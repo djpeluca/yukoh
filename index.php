4a7b9068aa22379162c5574d17eb8b13ac885c16
djpeluca <adrianbs@gmail.com>
2014-10-13 00:57:09 -0200

    commit old files and javascript modal fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta property="og:url" content="http://www.yukohfilms.com">
     <meta property="og:image" content="http://www.yukohfilms.com/assets/images/yukohfilms.jpg">
 	
-    <title>Yukoh Films</title>
+    <title>yukoh films</title>
 
 	<link rel="shortcut icon" href="assets/images/favicon.ico">
 	
@@ -20,11 +20,15 @@
 	<!-- Custom styles -->
 	<link rel="stylesheet" href="assets/css/yukoh.css">
     <?php
-    $i = rand(1, 28); // generate random number size of the array 
+    $i = rand(1, 27); // generate random number size of the array 
     ?>
     <style>
         html {
-	       background-image: url(assets/images/backgrounds/body<?php echo $i; ?>.jpg); 
+	       background-image: url(assets/images/backgrounds/body<?php echo $i; ?>.jpg);
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
         }
     </style>
 
@@ -39,9 +43,7 @@
 <nav class="mainmenu">
     <!-- Header Logo -->
     <div class="container logo">
-        <div class="row">
-            <img class="logo" src="assets/images/yukoh-white.png" />
-        </div>
+        <img class="logo" src="assets/images/yukoh-white.png" />
     </div>
 	<div class="container menu">
 		<div class="dropdown open">
@@ -60,21 +62,27 @@
 <section class="section" id="head">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a class="videothumb" title="Image 1" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/104191747.jpg" id="104191747" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/11168959.jpg" id="11168959" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6" ><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/103864773.jpg" id="103864773" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/28521836.jpg" id="28521836" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/35299491.jpg" id="35299491" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/36234305.jpg" id="36234305" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/21925430.jpg" id="21925430" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/42674191.jpg" id="42674191" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/48704395.jpg" id="48704395" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/49933877.jpg" id="49933877" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/65479167.jpg" id="65479167" title="yukoh! films"></a></div>
-			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><img class="thumbnail img-responsive" src="assets/images/videos/25051839.jpg" id="25051839" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6"><a class="videothumb" title="Image 1" href="#"><span class="videsc">yukoh manifiesto</span><img class="thumbnail img-responsive" src="assets/images/videos/104191747.jpg" id="104191747" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">spacecup bumper</span><img class="thumbnail img-responsive" src="assets/images/videos/106065466.jpg" id="106065466" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6" ><a title="yukoh! films" href="#"><span class="videsc">castel 3:54</span><img class="thumbnail img-responsive" src="assets/images/videos/105278859.jpg" id="105278859" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">la terraza in bloom</span><img class="thumbnail img-responsive" src="assets/images/videos/105949521.jpg" id="105949521" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">drugs tour</span><img class="thumbnail img-responsive" src="assets/images/videos/36234305.jpg" id="36234305" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">visual reel</span><img class="thumbnail img-responsive" src="assets/images/videos/65479167.jpg" id="65479167" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">hple - calor en el pecho</span><img class="thumbnail img-responsive" src="assets/images/videos/11168959.jpg" id="11168959" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">gaspar gaspar</span><img class="thumbnail img-responsive" src="assets/images/videos/28521836.jpg" id="28521836" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">reino plastico</span><img class="thumbnail img-responsive" src="assets/images/videos/42674191.jpg" id="42674191" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">el ocaso</span><img class="thumbnail img-responsive" src="assets/images/videos/48704395.jpg" id="48704395" title="yukoh! films"></a></div>
+			<div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">hple - el ciervo</span><img class="thumbnail img-responsive" src="assets/images/videos/17974049.jpg" id="17974049" title="yukoh! films"></a></div>
+            <div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">toyota franky</span><img class="thumbnail img-responsive" src="assets/images/videos/21925430.jpg" id="21925430" title="yukoh! films"></a></div>
+            <div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">hobby metal</span><img class="thumbnail img-responsive" src="assets/images/videos/21981222.jpg" id="21981222" title="yukoh! films"></a></div>
+            <div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">d&iacute;a del cine</span><img class="thumbnail img-responsive" src="assets/images/videos/21960380.jpg" id="21960380" title="yukoh! films"></a></div>
+            <div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">christian delpech</span><img class="thumbnail img-responsive" src="assets/images/videos/35299491.jpg" id="35299491" title="yukoh! films"></a></div>
+            <div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">bendita eres tristeza</span><img class="thumbnail img-responsive" src="assets/images/videos/49933877.jpg" id="49933877" title="yukoh! films"></a></div>
+            <div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">tcm - gol</span><img class="thumbnail img-responsive" src="assets/images/videos/21957305.jpg" id="21957305" title="yukoh! films"></a></div>
+            <div class="col-lg-3 col-sm-4 col-xs-6"><a title="yukoh! films" href="#"><span class="videsc">clasificados.pr</span><img class="thumbnail img-responsive" src="assets/images/videos/21939812.jpg" id="21939812" title="yukoh! films"></a></div>
 		</div> <!-- /row -->
 	</div>
-    <p class="copyright">® Yukoh Films / contact@yukohfilms.com</p>
+    <p class="copyright">© yukoh films / contact@yukohfilms.com / <a rel="nofollow" target="_blank" href="http://vimeo.com/yukoh"><img width="40px" height="auto" src="assets/images/vimeo-logo.png" alt="vimeo" /></a> <a rel="nofollow" target="_blank" href="https://twitter.com/maxcontenti"><img width="40px" height="auto" src="assets/images/Twitter-logo.png" alt="twitter" /></a> <a rel="nofollow" target="_blank" href="https://www.facebook.com/pages/yukoh-films/523735727651861"><img width="40px" height="auto" src="assets/images/fb-logo.png" alt="facebook" /></a></p>
 	
 	<div class="modal" id="myModal" role="dialog">
 		  <div class="modal-dialog">
@@ -82,9 +90,8 @@
 			<div class="modal-body">
 				<div id="modalCarousel" class="carousel">
 				  <div class="carousel-inner">
-				  </div>
-				  <a class="carousel-control left" href="#modalCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-				  <a class="carousel-control right" href="#modalCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+				    <div class="item active" title="Image 1"><iframe class="videoframe" src="//player.vimeo.com/video/104191747?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/106065466?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/105278859?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/105949521?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/36234305?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/65479167?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/11168959?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/28521836?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/42674191?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/48704395?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/17974049?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/21925430?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/21981222?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/21960380?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/35299491?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/49933877?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/21957305?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div><div class="item" title="yukoh! films"><iframe class="videoframe" src="//player.vimeo.com/video/21939812?byline=0&amp;portrait=0&amp;color=ff5a60" width="960" height="540" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+                    </div>
 				</div>
 			</div>
 		   </div>
@@ -98,15 +105,15 @@
 	
 		<h2 class="text-center title">about</h2>
 		<div class="row">
-			<div class="col-sm-4 col-sm-offset-2">    
+			<div class="col-sm-6">    
 				<p>These are letters. You are reading them. You are watching shapes. This is the construct of the mind. 
-                    I limit the brain imposes you to understand a creation of itself. You are witnessing a fucking lie. The Truth isn´t taken, isn´t 
-                    given, isn´t found. It exists all around us. It´s you. It´s me. It is everything that is. We beileve in truth, and we make true
+                    A limit the brain imposes you to understand a creation of itself. You are witnessing a fucking lie. The Truth isn´t taken, isn´t 
+                    given, isn´t found. It exists all around us. It´s you. It´s me. It is everything that is. We believe in truth, and we make true
                     work.
                 </p> 
                 <h5><strong>We are Yukoh Films</strong></h5>
 			</div>
-			<div class="col-sm-4">  
+			<div class="col-sm-6">  
 				<p>Estas son letras. Las estas leyendo. Estas viendo formas. Esta es una construcción de la mente.
 Un limite que el cerebro te impone para entender su propia creación. Estas viendo una puta mentira.
 La verdad no se toma, no se da, no se encuentra.
@@ -124,14 +131,15 @@ Nosotros creemos en la verdad, y hacemos trabajo verdadero.</p>
 	
 		<h2 class="text-center title">contact</h2>
 		<div class="row">
-			<div class="col-lg-4 contact">    
-				<h3><strong>Maximiliano Contenti - Director - Founder<br></strong></h3>
-                <h5><strong>contact phone<br></strong></h5>
-				<p>+598 091 438916</p> 
-                <h5><strong>contact mail<br></strong></h5>    
+			<div class="contact">    
+				<h3><strong>Maximiliano Contenti</strong></h3>
+                <h3 style="margin-top: 5px;"><strong>Director - Founder<br></strong></h3>
+                <h4><strong>contact phone<br></strong></h4>
+				<p><a style="text-decoration:none;" tel:"+598091438916">+598 091 438916</a></p> 
+                <h4><strong>contact mail<br></strong></h4>    
 				<p>contact@yukohfilms.com</p>
-                <h5><strong>links<br></strong></h5>    
-				<p><a rel="nofollow" target="_blank" href="http://vimeo.com/yukoh">Vimeo</a> / <a rel="nofollow" target="_blank" href="https://twitter.com/maxcontenti">Twitter</a> / <a rel="nofollow" target="_blank" href="https://www.facebook.com/pages/yukoh-films/523735727651861">Facebook</a></p>
+                <h4><strong>social links<br></strong></h4>    
+				<p><a rel="nofollow" target="_blank" href="http://vimeo.com/yukoh"><img width="40px" height="auto" src="assets/images/vimeo-logo.png" alt="vimeo" /></a> <a rel="nofollow" target="_blank" href="https://twitter.com/maxcontenti"><img width="40px" height="auto" src="assets/images/Twitter-logo.png" alt="twitter" /></a> <a rel="nofollow" target="_blank" href="https://www.facebook.com/pages/yukoh-films/523735727651861"><img width="40px" height="auto" src="assets/images/fb-logo.png" alt="facebook" /></a></p>
 			</div>
 		</div>
 	</div>
@@ -147,8 +155,8 @@ Nosotros creemos en la verdad, y hacemos trabajo verdadero.</p>
 <script type='text/javascript'>
         
 $(document).ready(function() {
-        
-    /* copy loaded thumbnails into carousel */
+    /*Comented section to fix******
+    /* copy loaded thumbnails into carousel 
     $('.row .thumbnail').on('load', function() {
 
     }).each(function(i) {
@@ -158,7 +166,7 @@ $(document).ready(function() {
         var title = $(this).parent('a').attr("title");
           
           
-        /*fix for vimeo frames */
+        /*fix for vimeo frames 
         var frame = "<iframe class=\"videoframe\" src=\"//player.vimeo.com/video/" + $(this).attr('id') + "?byline=0&amp;portrait=0&amp;color=ff5a60\" width=\"960\" height=\"540\"   frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
 
         item.attr("title",title);
@@ -169,6 +177,7 @@ $(document).ready(function() {
         }
       }
     });
+    comented section to fix until here  */
 
     /* activate the carousel */
     $('#modalCarousel').carousel({interval:false});
@@ -185,7 +194,31 @@ $(document).ready(function() {
         $('#myModal').modal('show'); // show the modal
         $('#modalCarousel').carousel(id); // slide carousel to selected
     });
+    /* when clicking the span */
+    $('.row .videsc').click(function(){
+        var idx = $(this).parents('div').index();
+        var id = parseInt(idx);
+        $('#myModal').modal('show'); // show the modal
+        $('#modalCarousel').carousel(id); // slide carousel to selected
+    });
+    /*when hover the div show the span*/
+    $('#head img').hover(function(){
+            $(this).prev().show();
+    });
+    $('#head .videsc').mouseleave(function(){
+        $(this).hide();    
+    });
 });
+
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54730056-1', 'auto');
+  ga('send', 'pageview');
 
 </script>
        <!-- Yukoh! template from djpeluca - http://djpeluca.com is based on single-page Bootstrap template  Magister from  **Sergey Pozhilov** - http://pozhilov.com
